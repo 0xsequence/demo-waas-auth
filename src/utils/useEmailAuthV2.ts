7bdbfdb4facabcf90c2edf3cf7226b6b5df6ca97
Patryk Kalinowski <patryk@kalinowski.dev>
2024-07-04 16:51:46 +0200

    Use the new signIn for email login

--- src/utils/useEmailAuthV2.ts
+++ src/utils/useEmailAuthV2.ts
@@ -1,49 +1,44 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import { sequence } from '../main'
-import { Challenge } from '@0xsequence/waas';
-
 
 export function useEmailAuthV2({ onSuccess, sessionName }: { onSuccess: (res: { wallet: string, sessionId: string }) => void, sessionName: string }) {
   const [error, setError] = useState<unknown>()
   const [loading, setLoading] = useState(false)
-  const [challenge, setChallenge] = useState<Challenge | null>(null)
+  const [inProgress, setInProgress] = useState(false)
+  const [respondWithCode, setRespondWithCode] = useState<((code: string) => Promise<void>) | null>()
+
+  useEffect(() => {
+    return sequence.onEmailAuthCodeRequired(async (respondWithCode) => {
+      setLoading(false)
+      setRespondWithCode(() => respondWithCode)
+    })
+  }, [sequence, setLoading, setRespondWithCode])
 
   const initiateAuth = async (email: string) => {
     setLoading(true)
-
+    setInProgress(true)
     try {
-      const challenge = await sequence.initAuth({ email })
-      setChallenge(challenge)
-    } catch (e: any) {
-      console.error(e)
-      setError(e.message || "Unknown error")
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  const sendChallengeAnswer = async (answer: string) => {
-    if (!challenge) {
-      throw new Error('email challenge not started yet')
-    }
-
-    setLoading(true)
-
-    try {
-      const res = await sequence.completeAuth(challenge.withAnswer(answer), { sessionName })
+      const res = await sequence.signIn({ email }, sessionName)
       onSuccess(res)
     } catch (e: any) {
       setError(e.message || "Unknown error")
     } finally {
       setLoading(false)
+      setInProgress(false)
+    }
+  }
+
+  const sendChallengeAnswer = async (answer: string) => {
+    if (respondWithCode) {
+      await respondWithCode(answer)
     }
   }
 
   return {
-    inProgress: loading || !!challenge,
+    inProgress,
+    initiateAuth,
     loading,
     error,
-    initiateAuth,
-    sendChallengeAnswer: challenge ? sendChallengeAnswer : undefined,
+    sendChallengeAnswer: inProgress ? sendChallengeAnswer : undefined,
   }
 }
