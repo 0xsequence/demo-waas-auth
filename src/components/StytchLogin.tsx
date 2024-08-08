e92dc7620e3e17af98ceb087fb854fa34d1047ee
Patryk Kalinowski <pkal@horizon.io>
2024-08-08 20:13:43 +0200

    fix issues with StytchLogin (#30)

--- src/components/StytchLogin.tsx
+++ src/components/StytchLogin.tsx
@@ -5,37 +5,51 @@ import {randomName} from "../utils/indexer.ts";
 import {SessionDurationOptions} from "@stytch/vanilla-js";
 import {Box, Button, Text, TextInput} from "@0xsequence/design-system";
 
+enum StytchLoginState {
+  NONE,
+  AUTH_INITIATED,
+  GOT_MAGIC_LINK,
+  SEQUENCE_SIGN_IN,
+}
+
 export function StytchLogin() {
   const stytchClient = useStytch()
   const { session: stytchSession } = useStytchSession();
+  const [state, setState] = useState(StytchLoginState.NONE)
 
   const [stytchEmail, setStytchEmail] = useState('')
   const stytchInputRef = useRef<HTMLInputElement | null>(null)
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
-    if (params.get('stytch_token_type') === 'magic_links') {
-      (async () => {
-        if (stytchSession) {
-          const tokens = stytchClient.session.getTokens()!
-          console.log({ tokens })
-          const walletAddress = await sequence.signIn({
-            idToken: tokens.session_jwt,
-          }, randomName())
-
-          console.log(`Wallet address: ${walletAddress}`)
-          router.navigate('/')
-        } else {
-          await new Promise((resolve) => setTimeout(resolve, 5000))
-          await stytchClient.magicLinks.authenticate(params.get('token') || '', {
-            session_duration_minutes: 5,
-          } as SessionDurationOptions)
-        }
+    if (params.get('stytch_token_type') === 'magic_links' && state === StytchLoginState.NONE) {
+      setState(StytchLoginState.GOT_MAGIC_LINK)
+      ;(async () => {
+        await stytchClient.magicLinks.authenticate(params.get('token') || '', {
+          session_duration_minutes: 5,
+        } as SessionDurationOptions)
       })()
     }
-  }, [stytchClient, stytchSession]);
+  }, [stytchClient, stytchSession, state]);
+
+  useEffect(() => {
+    if (stytchSession && state == StytchLoginState.GOT_MAGIC_LINK) {
+      setState(StytchLoginState.SEQUENCE_SIGN_IN)
+      ;(async () => {
+        const tokens = stytchClient.session.getTokens()!
+        const walletAddress = await sequence.signIn({
+          idToken: tokens.session_jwt,
+        }, randomName())
+
+        console.log(`Wallet address: ${walletAddress}`)
+        setState(StytchLoginState.NONE)
+        router.navigate('/')
+      })()
+    }
+  }, [stytchSession, stytchClient, state]);
 
   const initiateStytchEmailAuth = async (email: string) => {
+    setState(StytchLoginState.AUTH_INITIATED)
     await stytchClient.magicLinks.email.loginOrCreate(email, {})
   }
 
@@ -63,6 +77,7 @@ export function StytchLogin() {
             }}
             value={stytchEmail}
             placeholder="hello@example.com"
+            disabled={state !== StytchLoginState.NONE}
             required
           />
         </Box>
@@ -72,8 +87,14 @@ export function StytchLogin() {
               label="Continue"
               onClick={() => initiateStytchEmailAuth(stytchEmail)}
               data-id="continueButton"
+              disabled={state !== StytchLoginState.NONE}
             />
         </Box>
+        {state === StytchLoginState.AUTH_INITIATED && (
+          <Box>
+            <Text variant="normal" color="text80">Magic link sent to your email. Please click the link. You can close this page.</Text>
+          </Box>
+        )}
       </Box>
     </Box>
   )
